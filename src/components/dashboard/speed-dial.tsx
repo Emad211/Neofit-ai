53f0144e8d588ef1f9f37e55fd99b3856bf78166
emad karimi <emad.k50000@gmail.com>
2025-08-13 01:03:49 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button size="icon

--- src/components/dashboard/speed-dial.tsx
+++ src/components/dashboard/speed-dial.tsx
@@ -41,7 +41,7 @@ export function SpeedDial() {
             onClick={() => setIsOpen(false)}
         />
         
-        <div className="fixed bottom-6 right-6 md:bottom-8 md:right-8 z-50">
+        <div className="fixed bottom-24 right-6 md:bottom-28 md:right-8 z-50">
             <div className="relative flex flex-col items-end gap-4">
                 {/* Action Buttons */}
                 <div
